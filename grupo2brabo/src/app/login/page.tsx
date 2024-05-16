c5224fe916bf64854517d0835f68b58b43b59c21
Giovana <velascopachecogiovana@gmail.com>
2024-05-16 00:52:41 -0300

    :sparkles: nova funcionalidade:imagem na tela de login

--- grupo2brabo/src/app/login/page.tsx
+++ grupo2brabo/src/app/login/page.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 export default function login(): JSX.Element {
     return(
-        <div></div>
+        <div className="w-screen h-screen bg-[#EDEDED]">
+           <img className="w-2/4 h-screen" src="/imagem.png"></img> 
+        </div>
     );
 };
